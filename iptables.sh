168eeda26488dfd5199ce4c0b6077328780c2e32
jay <jay@dev.local>
2014-09-20 19:17:10 -0400

    working on ifconfig for centos7

--- iptables.sh
+++
@@ -1,4 +0,0 @@
-iptables --table nat --append POSTROUTING --out-interface ppp0 -j MASQUERADE
-iptables -I INPUT -s 10.0.0.0/8 -i ppp0 -j ACCEPT
-iptables --append FORWARD --in-interface eth0 -j ACCEPT
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE && iptables-save
